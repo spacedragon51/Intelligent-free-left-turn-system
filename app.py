891752ddab6094381aa34aa87396ccdfdcee1491
spacedragon51 <elisharaj79@gmail.com>
2026-09-10 13:16:49 +0530

    feat: Add logo asset and update page icon in Streamlit app

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import logging
 # Page config
 st.set_page_config(
     page_title="Intelligent Free Left Turn Management",
-    page_icon="",
+    page_icon="./assets/logo.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
